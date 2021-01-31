0b1d366ff523602ff336c688eba3abaaea6af8bc
Michel Joaquim <66667178+MIchelJoaquim@users.noreply.github.com>
2021-01-31 05:11:15 +0100

    jest init

---
+++ src/tests/user.spec.ts
@@ -0,0 +1,7 @@
+
+import { userDTO } from '../user/userDTO'
+
+test('it should be ok', () => {
+  const user: userDTO = { nome: 'Michel', email: 'michel@gmail.com' }
+  expect(user.nome).toEqual('Michel')
+})
